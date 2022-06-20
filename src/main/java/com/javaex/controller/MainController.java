c6614326d6b515ced4bf59be4329c5e24d6d99d5
joyunju <whdbswn1113@naver.com>
2022-06-21 04:12:41 +0900

    test

---
+++ src/main/java/com/javaex/controller/MainController.java
@@ -0,0 +1,26 @@
+package com.javaex.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+	// 필드
+
+	// 생성자
+
+	// 메소드 - gs
+
+	// 메소드 일반
+	@RequestMapping(value = "/test", method = { RequestMethod.GET, RequestMethod.POST })
+	public String test() {
+		System.out.println("MainController>test()");
+		
+		//기본 뷰 리졸버 하기 전 
+		//return "/WEB-INF/views/tes.jsp";
+		return "test";
+	}
+
+}
