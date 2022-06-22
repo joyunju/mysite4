4427cd681941e7f74ef8730e302b441e1091a389
joyunju <whdbswn1113@naver.com>
2022-06-22 15:56:07 +0900

    userInsert

---
+++ src/main/java/com/javaex/dao/UserDao.java
@@ -0,0 +1,20 @@
+package com.javaex.dao;
+
+import com.javaex.vo.UserVo;
+
+public class UserDao {
+
+	// 필드
+
+	// 생성자 - 디폴트 생성자 
+
+	// 메소드 - gs
+
+	// 메소드 일반
+	public void userInsert(UserVo userVo) {
+		System.out.println("UserDao > userInsert()");
+		
+		System.out.println(userVo);
+	}
+
+}
