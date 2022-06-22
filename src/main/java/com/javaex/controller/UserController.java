c92f97940b74e4f9fb85142442a79fb7cc6ca7e5
joyunju <whdbswn1113@naver.com>
2022-06-22 15:45:57 +0900

    @Autowired

--- src/main/java/com/javaex/controller/UserController.java
+++ src/main/java/com/javaex/controller/UserController.java
@@ -1,16 +1,21 @@
 package com.javaex.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.javaex.service.UserService;
 import com.javaex.vo.UserVo;
 
 @Controller
 public class UserController {
 
 	// 필드
+	@Autowired
+	// UserService userService = new UserService();
+	UserService userService;
 
 	// 생성자
 
@@ -34,6 +39,9 @@ public class UserController {
 		System.out.println("UserController > join()");
 
 		System.out.println(userVo);
+
+		// UserService userService = new UserService();
+		userService.join(userVo);
 
 		// 기본 뷰 리졸버 하기 전
 		// return "/WEB-INF/views/user/join.jsp";
