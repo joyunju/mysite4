08f9e14e9a195e2c44912ed4f2d16505cc45ad24
joyunju <whdbswn1113@naver.com>
2022-06-28 11:12:51 +0900

    controller

---
+++ src/main/java/com/javaex/api/controller/ApiGuestbookController.java
@@ -0,0 +1,28 @@
+package com.javaex.api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ApiGuestbookController {
+	
+	// 필드
+
+		// 생성자
+
+		// 메소드 - gs
+
+		// 메소드 일반
+
+		// 메인
+		@RequestMapping(value = "/api/guestbook/addList", method = { RequestMethod.GET, RequestMethod.POST })
+		public String addList() {
+			System.out.println("ApiGuestbookController > addList");
+			
+			// 기본 뷰 리졸버 하기 전
+			// return "/WEB-INF/views/apiGuestbook/addList.jsp";
+			return "apiGuestbook/addList";
+		}
+
+}
