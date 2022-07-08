ce5a265ad5a571aff404fde7fc78a5501a1ed434
joyunju <whdbswn1113@naver.com>
2022-07-08 18:22:20 +0900

    public list

--- src/main/java/com/javaex/api/controller/ApiGuestbookController.java
+++ src/main/java/com/javaex/api/controller/ApiGuestbookController.java
@@ -1,30 +1,42 @@
 package com.javaex.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.javaex.service.GuestbookService;
+
 @Controller
 public class ApiGuestbookController {
-	
+
 	// 필드
+	@Autowired
+	private GuestbookService guestbookService;
 
-		// 생성자
+	// 생성자
 
-		// 메소드 - gs
+	// 메소드 - gs
 
-		// 메소드 일반
+	// 메소드 일반
 
-		// 메인
-		// http://localhost:8088/mysite/api/guestbook/addList
-		@RequestMapping(value = "/api/guestbook/addList", method = { RequestMethod.GET, RequestMethod.POST })
-		public String addList() {
-			System.out.println("ApiGuestbookController > addList");
+	// 메인
+	// http://localhost:8088/mysite/api/guestbook/addList
+	@RequestMapping(value = "/api/guestbook/addList", method = { RequestMethod.GET, RequestMethod.POST })
+	public String addList() {
+		System.out.println("ApiGuestbookController > addList()");
+
+		// 기본 뷰 리졸버 하기 전
+		// return "/WEB-INF/views/apiGuestbook/addList.jsp";
+		return "apiGuestbook/addList";
+	}
+
+	// 방명록 리스트 데이터만 보내줘
+	@RequestMapping(value = "/api/guestbook/list", method = { RequestMethod.GET, RequestMethod.POST })
+		public String list() {
+		System.out.println("ApiGuestbookController > list()");
 			
-			
-			// 기본 뷰 리졸버 하기 전
-			// return "/WEB-INF/views/apiGuestbook/addList.jsp";
-			return "apiGuestbook/addList";
-		}
+		return "";
+	}
 
 }
