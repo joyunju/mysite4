54a833774f8aed7921d6e47795dbae3f701fe1a2
joyunju <whdbswn1113@naver.com>
2022-07-08 18:22:00 +0900

    GuestbookService

---
+++ src/main/java/com/javaex/service/GuestbookService.java
@@ -0,0 +1,17 @@
+package com.javaex.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.javaex.vo.GuestbookVo;
+
+@Service
+public class GuestbookService {
+	
+	public List<GuestbookVo> getGuestList(){
+		
+		return null;
+	}
+
+}
