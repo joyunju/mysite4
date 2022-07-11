e6d88fb3b5ed12caccd5d9fe0570c30a6c4b7f45
joyunju <whdbswn1113@naver.com>
2022-07-11 17:13:51 +0900

    guestbookDao.selectList();

--- src/main/java/com/javaex/service/GuestbookService.java
+++ src/main/java/com/javaex/service/GuestbookService.java
@@ -2,14 +2,22 @@ package com.javaex.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.javaex.dao.GuestBookDao;
 import com.javaex.vo.GuestbookVo;
 
 @Service
 public class GuestbookService {
 	
+	@Autowired
+	private GuestBookDao guestbookDao;
+	
 	public List<GuestbookVo> getGuestList(){
+		System.out.println("GuestbookService > getGuestList()");
+		
+		guestbookDao.selectList();
 		
 		return null;
 	}
